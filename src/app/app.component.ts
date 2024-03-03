11e6bcfc2f15c25ea26b93b68ef5c0cbe295439b
HelioFernandes404 <heliojobs25@gmail.com>
2024-03-03 17:44:09 -0300

    feat: iniciando projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+      RouterOutlet, 
+      RouterLink, 
+      RouterLinkActive,
+      MatButtonModule,
+      MatIconModule,
+      MatToolbarModule
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'app-3win';
+    title = 'app-3win';
 }
---
+++ src/app/appwins/appwins.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appwins',
+  standalone: true,
+  imports: [],
+  templateUrl: './appwins.component.html',
+  styleUrl: './appwins.component.css'
+})
+export class AppwinsComponent {
+
+}
